1fde12cab37d6a834286e1ab705acd7a3a815132
Álvaro Gascón <alvarogascon@MBP-de-Alvaro.local>
2023-12-08 13:10:31 +0100

    first commit

---
+++ src/blackjack/usecases/index.js
@@ -0,0 +1,5 @@
+export { crearDeck } from './crear-deck';
+export { pedirCarta } from './pedir-carta';
+export { turnoComputadora } from './turnoComputadora';
+export { valorCarta } from './valor-carta';
+export {crearCartaHTML} from './crear-carta-html';
---
+++ src/blackjack/usecases/pedir-carta.js
@@ -0,0 +1,14 @@
+
+/**
+ * Esta función permite tomar una carta
+ * @param {Array<Strings>} deck es un arreglo de string
+ * @returns {String} retorna la carta del deck
+ */
+export const pedirCarta = ( deck ) => {
+
+  if ( !deck || deck.length === 0 ) {
+      throw new Error ('No hay cartas en el deck');
+  }
+  const carta = deck.pop();
+  return carta;
+}
